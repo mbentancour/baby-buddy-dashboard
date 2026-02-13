d93020a02bcd31f483790408e6db9c7c9e20b3d3
Martin Bentancour <martin@viakube.com>
2026-02-13 16:42:49 +0100

    Actually use the unit setting

--- baby-buddy-dashboard/run.sh
+++ baby-buddy-dashboard/run.sh
@@ -4,6 +4,8 @@
 export BABY_BUDDY_URL=$(bashio::config 'baby_buddy_url')
 export BABY_BUDDY_API_KEY=$(bashio::config 'baby_buddy_api_key')
 export REFRESH_INTERVAL=$(bashio::config 'refresh_interval')
+export DEMO_MODE=$(bashio::config 'demo_mode')
+export UNIT_SYSTEM=$(bashio::config 'unit_system')
 
 bashio::log.info "Starting Baby Buddy Dashboard..."
 bashio::log.info "Connecting to Baby Buddy at: ${BABY_BUDDY_URL}"
